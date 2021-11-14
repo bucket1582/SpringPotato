a0afafa2c8830672ceabec725c65fa8be63dac8a
Bucket1572 <sout0811@naver.com>
2021-11-15 02:06:33 +0900

    Fixed: 색깔 수정; v0.4.1; 테스트 게임 통과

--- src/main/kotlin/com/github/bucket1572/springpotato/common/colors/ColorTag.kt
+++ src/main/kotlin/com/github/bucket1572/springpotato/common/colors/ColorTag.kt
@@ -13,7 +13,7 @@ enum class ColorTag(val red: Int, val green: Int, val blue: Int) {
     TYPE_DESCRIPTION(81, 89, 232),
     LIMITATION_DESCRIPTION(255, 0, 0),
     SCOREBOARD(255, 215, 0),
-    PHASE(0, 0, 0)
+    PHASE(255, 255, 255)
 }
 
 fun ColorTag.getTextColor(): TextColor = TextColor.color(red, green, blue)
