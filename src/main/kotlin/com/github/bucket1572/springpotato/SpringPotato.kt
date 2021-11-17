4092bca83896e523afd095f271241453ffce7e66
Bucket1572 <sout0811@naver.com>
2021-11-17 23:35:17 +0900

    Feat: 스폰 반경 조정; 커맨드 Validation 추가

--- src/main/kotlin/com/github/bucket1572/springpotato/SpringPotato.kt
+++ src/main/kotlin/com/github/bucket1572/springpotato/SpringPotato.kt
@@ -1,10 +1,10 @@
 package com.github.bucket1572.springpotato
 
 import com.github.bucket1572.springpotato.basic_logic.handlers.GameHandler
+import com.github.bucket1572.springpotato.common.text_components.AlertComponent
 import com.github.bucket1572.springpotato.wand.handlers.WandHandler
 import io.github.monun.kommand.getValue
 import io.github.monun.kommand.kommand
-import org.bukkit.GameRule
 import org.bukkit.plugin.java.JavaPlugin
 
 class SpringPotato : JavaPlugin() {
@@ -29,7 +29,7 @@ class SpringPotato : JavaPlugin() {
                 then("mainGameTime" to int(minimum = 0)) {
                     then("buzzerBeaterTime" to int(minimum = 0)) {
                         then("lastHandoutTime" to int(minimum = 0)) {
-                            then("playRadius" to int(minimum = 0)) {
+                            then("playDiameter" to int(minimum = 0)) {
                                 requires {
                                     (this.isPlayer && this.isOp) || (this.isConsole)
                                 }
@@ -38,9 +38,18 @@ class SpringPotato : JavaPlugin() {
                                     val mainGameTime: Int by it
                                     val buzzerBeaterTime: Int by it
                                     val lastHandoutTime: Int by it
-                                    val playRadius: Int by it
+                                    val playDiameter: Int by it
+
+                                    // Validation
+                                    if (playDiameter < GameHandler.SPAWN_RADIUS * 2) {
+                                        this.player.sendMessage(
+                                            AlertComponent("게임 구역은 최소 ${GameHandler.SPAWN_RADIUS * 2}의 직경을 가지고 있어야 합니다.").getComponent()
+                                        )
+                                        return@executes
+                                    }
+
                                     GameHandler.changeToMainPhase(
-                                        this@SpringPotato, server, this.player.location, playRadius,
+                                        this@SpringPotato, server, this.player.location, playDiameter,
                                         mainGameTime, buzzerBeaterTime, lastHandoutTime
                                     )
                                 }
